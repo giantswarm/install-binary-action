5be262d03ecfe0da04d45640cb355a090fd715eb
Paweł Kopiczko <pawel@giantswarm.io>
2020-10-20 10:23:09 +0100

    Add action code (#1)

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 8
+    }
+}
