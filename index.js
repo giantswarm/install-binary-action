c8c87bfe805275015cc6fcb0e865ec353d385a8d
Xoán Mallón <xmallon@zscaler.com>
2024-09-17 22:05:25 +0200

    chore: add related changes with transform

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
-import paths from 'path';
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const tc = require('@actions/tool-cache');
 
-import core from '@actions/core';
-import exec from '@actions/exec';
-import tc from '@actions/tool-cache';
+async function run() {
 
-const run = async () => {
   try {
-    const binary = core.getInput('binary');
-    const version = core.getInput('version');
+    let binary = core.getInput('binary');
+    let binaryNewName = core.getInput('binary_new_name');
+    let version = core.getInput('version');
     let downloadURL = core.getInput('download_url');
     let tarballBinaryPath = core.getInput('tarball_binary_path');
     let smokeTest = core.getInput('smoke_test');
 
-    const fillTemplate = str => 
-      str
-        .replace(/\$\{binary\}/ug, binary)
-        .replace(/\$\{version\}/ug, version);
+    const fillTemplate = function(s) {
+      s = s.replace(/\$\{binary\}/g, binary);
+      s = s.replace(/\$\{version\}/g, version);
+      return s;
+    }
 
     downloadURL = fillTemplate(downloadURL);
-    tarballBinaryPath = fillTemplate(tarballBinaryPath)
+    tarballBinaryPath = fillTemplate(tarballBinaryPath);
     smokeTest = fillTemplate(smokeTest);
-
-    core.info(`download URL:         ${downloadURL}`)
-    core.info(`tarball binary path:  ${tarballBinaryPath}`)
-    core.info(`smoke test:           ${smokeTest}`)
+    core.info(`binary:               ${binary}`);
+    if (binaryNewName) {
+      core.info(`binaryNewName:        ${binaryNewName}`);
+    }
+    core.info(`download URL:         ${downloadURL}`);
+    core.info(`tarball binary path:  ${tarballBinaryPath}`);
+    core.info(`smoke test:           ${smokeTest}`);
 
     const stripComponents = tarballBinaryPath.split("/").length - 1;
 
-    await installTool(binary, version, downloadURL, stripComponents, tarballBinaryPath);
+    await installTool(binary, version, downloadURL, stripComponents, tarballBinaryPath, binaryNewName);
     await exec.exec(smokeTest);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-const installTool = async (name, version, url, stripComponents, wildcard) => {
+async function getUnTarCommand(name, path, stripComponents, wildcard, binaryNewName) {
+  let command = `tar -C ${name} -xzvf ${path} --strip-components ${stripComponents} --wildcards ${wildcard}`;
+  if (binaryNewName) {
+    command = command + ` --transform=s/${wildcard}/${binaryNewName}/`;
+  }
+  return command
+}
+
+async function installTool(name, version, url, stripComponents, wildcard, binaryNewName) {
   let cachedPath = tc.find(name, version);
   if (cachedPath) {
     core.addPath(cachedPath);
-    return;
+    return
   }
 
   const path = await tc.downloadTool(url);
-
   await exec.exec(`mkdir ${name}`);
-  if (paths.extname(url) === '') {
-    // If there is not extension, assume this is an unarchived binary.
-    await exec.exec(`mv "${path}" "${name}/${name}"`);
-    await exec.exec(`chmod +x "${name}/${name}"`);
-  } else {
-    await exec.exec(`tar -C ${name} -xzvf ${path} --strip-components ${stripComponents} --wildcards ${wildcard}`);
-  }
+  let unTarCommand = await getUnTarCommand(name, path, stripComponents, wildcard, binaryNewName);
+  await exec.exec(`${unTarCommand}`);
 
   cachedPath = await tc.cacheDir(name, name, version);
   core.addPath(cachedPath);
