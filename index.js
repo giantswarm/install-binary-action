giantswarm/install-binary-action

commit 580414b9aec10941a2f1dc9ce5da4b85903711d5

Add binary_new_name input (#236)
Co-authored-by: Xoán Mallón <xmallon@zscaler.com>

index.js
@@ -1,5 +1,3 @@
-import paths from 'path';
-
 import core from '@actions/core';
 import exec from '@actions/exec';
 import tc from '@actions/tool-cache';
@@ -8,6 +6,7 @@ const run = async () => {
   try {
     const binary = core.getInput('binary');
     const version = core.getInput('version');
+    const binaryNewName = core.getInput('binary_new_name');
     let downloadURL = core.getInput('download_url');
     let tarballBinaryPath = core.getInput('tarball_binary_path');
     let smokeTest = core.getInput('smoke_test');
@@ -21,36 +20,42 @@ const run = async () => {
     tarballBinaryPath = fillTemplate(tarballBinaryPath)
     smokeTest = fillTemplate(smokeTest);
 
+    core.info(`binary:               ${binary}`);
+    if (binaryNewName) {
+      core.info(`binaryNewName:        ${binaryNewName}`);
+    }
     core.info(`download URL:         ${downloadURL}`)
     core.info(`tarball binary path:  ${tarballBinaryPath}`)
     core.info(`smoke test:           ${smokeTest}`)
 
     const stripComponents = tarballBinaryPath.split("/").length - 1;
 
-    await installTool(binary, version, downloadURL, stripComponents, tarballBinaryPath);
+    await installTool(binary, version, downloadURL, stripComponents, tarballBinaryPath, binaryNewName);
     await exec.exec(smokeTest);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-const installTool = async (name, version, url, stripComponents, wildcard) => {
+const getUnTarCommand = (name, path, stripComponents, wildcard, binaryNewName) => {
+  let command = `tar -C ${name} -xzvf ${path} --strip-components ${stripComponents} --wildcards ${wildcard}`;
+  if (binaryNewName) {
+    command += ` --transform=s/${wildcard}/${binaryNewName}/`;
+  }
+  return command;
+}
+
+const installTool = async (name, version, url, stripComponents, wildcard, binaryNewName) => {
   let cachedPath = tc.find(name, version);
   if (cachedPath) {
     core.addPath(cachedPath);
-    return;
+    return
   }
 
   const path = await tc.downloadTool(url);
-
   await exec.exec(`mkdir ${name}`);
-  if (paths.extname(url) === '') {
-    // If there is not extension, assume this is an unarchived binary.
-    await exec.exec(`mv "${path}" "${name}/${name}"`);
-    await exec.exec(`chmod +x "${name}/${name}"`);
-  } else {
-    await exec.exec(`tar -C ${name} -xzvf ${path} --strip-components ${stripComponents} --wildcards ${wildcard}`);
-  }
+  const unTarCommand = getUnTarCommand(name, path, stripComponents, wildcard, binaryNewName);
+  await exec.exec(`${unTarCommand}`);
 
   cachedPath = await tc.cacheDir(name, name, version);
   core.addPath(cachedPath);

dist/index.js
@@ -30318,23 +30318,21 @@ module.exports = parseParams
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
-/* harmony import */ var path__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(1017);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(2186);
-/* harmony import */ var _actions_exec__WEBPACK_IMPORTED_MODULE_2__ = __nccwpck_require__(1514);
-/* harmony import */ var _actions_tool_cache__WEBPACK_IMPORTED_MODULE_3__ = __nccwpck_require__(7784);
-
-
+/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(2186);
+/* harmony import */ var _actions_exec__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(1514);
+/* harmony import */ var _actions_tool_cache__WEBPACK_IMPORTED_MODULE_2__ = __nccwpck_require__(7784);
 
 
 
 
 const run = async () => {
   try {
-    const binary = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('binary');
-    const version = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('version');
-    let downloadURL = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('download_url');
-    let tarballBinaryPath = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('tarball_binary_path');
-    let smokeTest = _actions_core__WEBPACK_IMPORTED_MODULE_1__.getInput('smoke_test');
+    const binary = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('binary');
+    const version = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('version');
+    const binaryNewName = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('binary_new_name');
+    let downloadURL = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('download_url');
+    let tarballBinaryPath = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('tarball_binary_path');
+    let smokeTest = _actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput('smoke_test');
 
     const fillTemplate = str => 
       str
@@ -30345,42 +30343,47 @@ const run = async () => {
     tarballBinaryPath = fillTemplate(tarballBinaryPath)
     smokeTest = fillTemplate(smokeTest);
 
-    _actions_core__WEBPACK_IMPORTED_MODULE_1__.info(`download URL:         ${downloadURL}`)
-    _actions_core__WEBPACK_IMPORTED_MODULE_1__.info(`tarball binary path:  ${tarballBinaryPath}`)
-    _actions_core__WEBPACK_IMPORTED_MODULE_1__.info(`smoke test:           ${smokeTest}`)
+    _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`binary:               ${binary}`);
+    if (binaryNewName) {
+      _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`binaryNewName:        ${binaryNewName}`);
+    }
+    _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`download URL:         ${downloadURL}`)
+    _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`tarball binary path:  ${tarballBinaryPath}`)
+    _actions_core__WEBPACK_IMPORTED_MODULE_0__.info(`smoke test:           ${smokeTest}`)
 
     const stripComponents = tarballBinaryPath.split("/").length - 1;
 
-    await installTool(binary, version, downloadURL, stripComponents, tarballBinaryPath);
-    await _actions_exec__WEBPACK_IMPORTED_MODULE_2__.exec(smokeTest);
+    await installTool(binary, version, downloadURL, stripComponents, tarballBinaryPath, binaryNewName);
+    await _actions_exec__WEBPACK_IMPORTED_MODULE_1__.exec(smokeTest);
   } catch (error) {
-    _actions_core__WEBPACK_IMPORTED_MODULE_1__.setFailed(error.message);
+    _actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed(error.message);
   }
 }
 
-const installTool = async (name, version, url, stripComponents, wildcard) => {
-  let cachedPath = _actions_tool_cache__WEBPACK_IMPORTED_MODULE_3__.find(name, version);
+const getUnTarCommand = (name, path, stripComponents, wildcard, binaryNewName) => {
+  let command = `tar -C ${name} -xzvf ${path} --strip-components ${stripComponents} --wildcards ${wildcard}`;
+  if (binaryNewName) {
+    command += ` --transform=s/${wildcard}/${binaryNewName}/`;
+  }
+  return command;
+}
+
+const installTool = async (name, version, url, stripComponents, wildcard, binaryNewName) => {
+  let cachedPath = _actions_tool_cache__WEBPACK_IMPORTED_MODULE_2__.find(name, version);
   if (cachedPath) {
-    _actions_core__WEBPACK_IMPORTED_MODULE_1__.addPath(cachedPath);
-    return;
+    _actions_core__WEBPACK_IMPORTED_MODULE_0__.addPath(cachedPath);
+    return
   }
 
-  const path = await _actions_tool_cache__WEBPACK_IMPORTED_MODULE_3__.downloadTool(url);
+  const path = await _actions_tool_cache__WEBPACK_IMPORTED_MODULE_2__.downloadTool(url);
+  await _actions_exec__WEBPACK_IMPORTED_MODULE_1__.exec(`mkdir ${name}`);
+  const unTarCommand = getUnTarCommand(name, path, stripComponents, wildcard, binaryNewName);
+  await _actions_exec__WEBPACK_IMPORTED_MODULE_1__.exec(`${unTarCommand}`);
 
-  await _actions_exec__WEBPACK_IMPORTED_MODULE_2__.exec(`mkdir ${name}`);
-  if (path__WEBPACK_IMPORTED_MODULE_0__.extname(url) === '') {
-    // If there is not extension, assume this is an unarchived binary.
-    await _actions_exec__WEBPACK_IMPORTED_MODULE_2__.exec(`mv "${path}" "${name}/${name}"`);
-    await _actions_exec__WEBPACK_IMPORTED_MODULE_2__.exec(`chmod +x "${name}/${name}"`);
-  } else {
-    await _actions_exec__WEBPACK_IMPORTED_MODULE_2__.exec(`tar -C ${name} -xzvf ${path} --strip-components ${stripComponents} --wildcards ${wildcard}`);
-  }
-
-  cachedPath = await _actions_tool_cache__WEBPACK_IMPORTED_MODULE_3__.cacheDir(name, name, version);
-  _actions_core__WEBPACK_IMPORTED_MODULE_1__.addPath(cachedPath);
+  cachedPath = await _actions_tool_cache__WEBPACK_IMPORTED_MODULE_2__.cacheDir(name, name, version);
+  _actions_core__WEBPACK_IMPORTED_MODULE_0__.addPath(cachedPath);
 }
 
 run();
-
 })();
 

.github/workflows/test.yaml
@@ -5,6 +5,9 @@ on:
     branches:
       - master
 
+env:
+  AWS_NUKE_VERSION: 'v2.25.0'
+
 jobs:
   test:
     name: Test
@@ -28,7 +31,6 @@ jobs:
         with:
           binary: "devctl"
           version: "2.0.0"
-
       - name: Test downloading external binary
         uses: ./
         with:
@@ -37,11 +39,21 @@ jobs:
           download_url: "https://github.com/fsaintjacques/${binary}-tool/archive/${version}.tar.gz"
           tarball_binary_path: "*/src/${binary}"
           smoke_test: "${binary} --version"
-
-      - name: Test downloading external unarchived binary
+      - name: Set OS information related variables
+        run: |
+          echo "OS_NAME=$(uname -s | tr '[:upper:]' '[:lower:]')" >> $GITHUB_ENV
+          echo "OS_ARCH=$(dpkg --print-architecture)" >> $GITHUB_ENV
+          echo "OS_ARCH_TYPE=$(arch)" >> $GITHUB_ENV
+      - name: Set download aws-nuke download url and download binary name variables
+        run: |
+          echo "AWS_NUKE_DOWNLOAD_URL=https://github.com/rebuy-de/aws-nuke/releases/download/${{ env.AWS_NUKE_VERSION }}/aws-nuke-${{ env.AWS_NUKE_VERSION }}-${OS_NAME}-${OS_ARCH}.tar.gz" >> $GITHUB_ENV
+          echo "AWS_NUKE_DOWNLOAD_BINARY_NAME=aws-nuke-${{ env.AWS_NUKE_VERSION }}-${{ env.OS_NAME }}-${{ env.OS_ARCH }}" >> $GITHUB_ENV
+      - name: Test downloading aws-nuke
         uses: ./
         with:
-          binary: "vendir"
-          version: "0.40.2"
-          download_url: "https://github.com/carvel-dev/vendir/releases/download/v${version}/vendir-linux-amd64"
-          smoke_test: "${binary} --version"
+          binary: "aws-nuke"
+          version: "${{ env.AWS_NUKE_VERSION }}"
+          download_url: "${{ env.AWS_NUKE_DOWNLOAD_URL }}"
+          tarball_binary_path: "${{ env.AWS_NUKE_DOWNLOAD_BINARY_NAME }}"
+          smoke_test: "${binary} version"
+          binary_new_name: "aws-nuke"

action.yml
@@ -19,6 +19,9 @@ inputs:
     description: 'Template of the binary smoke test.'
     required: false
     default: '${binary} version'
+  binary_new_name:
+    description: 'Name of the binary once is installed. E.g. "aws"'
+    required: false
 outputs: {}
 runs:
   using: 'node20'

commit 76e899d52597e901ee3f51b0bdc36bc197733ea6

Restore downloading non-tar'ed binaries
The regression was introduced in PR https://github.com/giantswarm/install-binary-action/pull/236
where both the feature and the test were removed.

Signed-off-by: Matias Charriere <matias@giantswarm.io>

index.js
@@ -54,8 +54,15 @@ const installTool = async (name, version, url, stripComponents, wildcard, binary
 
   const path = await tc.downloadTool(url);
   await exec.exec(`mkdir ${name}`);
-  const unTarCommand = getUnTarCommand(name, path, stripComponents, wildcard, binaryNewName);
-  await exec.exec(`${unTarCommand}`);
+
+  if (paths.extname(url) === '') {
+    // If there is not extension, assume this is an unarchived binary.
+    await exec.exec(`mv "${path}" "${name}/${name}"`);
+    await exec.exec(`chmod +x "${name}/${name}"`);
+  } else {
+    const unTarCommand = getUnTarCommand(name, path, stripComponents, wildcard, binaryNewName);
+    await exec.exec(`${unTarCommand}`);
+  }
 
   cachedPath = await tc.cacheDir(name, name, version);
   core.addPath(cachedPath);